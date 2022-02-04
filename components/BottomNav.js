2df2f9358288f79e238669f319851e4ad073d6a9
Kevin Collazos <collazo-s@hotmail.com>
2022-02-03 20:14:59 -0500

    Prueba branch

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -1,27 +1,29 @@
 import { useState } from "react";
-import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+import {
+  Home as HomeIcon,
+  Favorite as FavoriteIcon,
+  Person as PersonIcon,
+} from "@mui/icons-material";
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = useState(0);
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <Box sx={{ width: "100%" }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      </BottomNavigation>
-    </Box>
+    <BottomNavigation
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0, width: "100%" }}
+      value={value}
+      showLabels
+      onChange={handleChange}
+    >
+      <BottomNavigationAction label="Inicio" icon={<HomeIcon />} />
+      <BottomNavigationAction label="Inventario" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Perfil" icon={<PersonIcon />} />
+    </BottomNavigation>
   );
 }
