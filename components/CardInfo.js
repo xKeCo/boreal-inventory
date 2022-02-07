54ab4e1e57ea26a10296580a0adcd95074f7ad80
Kevin Collazos <collazo-s@hotmail.com>
2022-02-06 19:20:39 -0500

    Cambio en el estilo de la presentacion de la informacion del paciente en el home

--- components/CardInfo.js
+++ components/CardInfo.js
@@ -1,23 +1,22 @@
 import React from "react";
 
 // Material UI
-import { Button, Collapse, Text } from "@nextui-org/react";
+import { Button, Collapse, Divider, Text } from "@nextui-org/react";
 
 export default function CardInfo({ id, name, doc, description }) {
   return (
     <>
-      <Collapse
-        key={id}
-        title={name}
-        subtitle={doc}
-        contentLeft={<Text>13:08pm</Text>}
-      >
+      <Collapse key={id} title={name} subtitle={doc}>
         {/* <p>{description}</p> */}
+        <Text h5>Hora: 13:08pm</Text>
+        <Divider css={{ m: "10px 0" }} />
         <Text>{description}</Text>
+        <Divider css={{ m: "10px 0" }} />
         <Text h4 css={{ mt: "1rem" }}>
           Pago: Si <br />
           Metodo: Transferencia
         </Text>
+
         <Button size="sm" css={{ bg: "#000", mt: "1rem", w: "100%" }}>
           Más info
         </Button>
